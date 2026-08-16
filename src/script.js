a01da400d35398528970ea84f2c6ea197b8cc316
meta <eloi.fil19@hotmail.fr>
2026-08-16 20:04:10 +0200

    Adjust Lenis smooth scroll duration to 0.8s

--- src/script.js
+++ src/script.js
@@ -196,7 +196,7 @@ renderGrid();
 /* ---------- Smooth Scroll (Lenis) ---------- */
 if (typeof Lenis !== 'undefined') {
   const lenis = new Lenis({
-    duration: 1,
+    duration: 0.8,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     smoothWheel: true,
   });
